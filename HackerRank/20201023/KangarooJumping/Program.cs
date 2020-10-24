deef005584024eb65056b892501e9c9628aaaac7
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-10-24 09:52:18 +0530

    2020-10-23 and 2020-10-24

---
+++ HackerRank/20201023/KangarooJumping/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Windows.Markup;
+
+class Solution
+{
+
+    // https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true
+
+    // You are choreographing a circus show with various animals. 
+    // For one act, you are given two kangaroos on a number line ready to jump in the positive direction (i.e, toward positive infinity).
+    // The first kangaroo starts at location  and moves at a rate of  meters per jump.
+    // The second kangaroo starts at location  and moves at a rate of  meters per jump.
+    // You have to figure out a way to get both kangaroos at the same location at the same time as part of the show.If it is possible, return YES, otherwise return NO.
+    // For example, kangaroo starts at with a jump distance and kangaroo starts at with a jump distance of . After one jump, they are both at, (, ), so our answer is YES.
+
+    // Complete the kangaroo function below.
+    static string kangaroo(int x1, int v1, int x2, int v2)
+    {
+        int jumps = 0;
+
+        while (x1 != x2)
+        {
+            // (x1 == x2) ==> means both first and second kangaroo are in same place
+
+            if ((x1 < x2) && (v1 <= v2))
+            {
+                // first kangaroo is behind and slower than second kangaroo -> so, can't catch up
+                return "NO";
+            }
+
+            if ((x2 < x1) && (v2 <= v1))
+            {
+                // second kangaroo is behind and slower than first kangaroo -> so, can't catch up
+                return "NO";
+            }
+
+            x1 += v1;
+            x2 += v2;
+            jumps++;
+        }
+
+        // both first and second kangaroo are in same place
+        return "YES";
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] x1V1X2V2 = Console.ReadLine().Split(' ');
+
+        int x1 = Convert.ToInt32(x1V1X2V2[0]);
+
+        int v1 = Convert.ToInt32(x1V1X2V2[1]);
+
+        int x2 = Convert.ToInt32(x1V1X2V2[2]);
+
+        int v2 = Convert.ToInt32(x1V1X2V2[3]);
+
+        // string result = kangaroo(x1, v1, x2, v2);
+
+        string result1 = kangaroo(0, 3, 4, 2);
+
+        textWriter.WriteLine(result1);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ HackerRank/20201024/SumOfDigitFactorials/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+    static long Factorial(long n)
+    {
+        if(n == 0)
+        {
+            return 1;
+        }
+
+        return n * Factorial(n - 1);
+    }
+
+    static long SumOfDigits(long n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+
+        int lastDigit = (int)(n % 10);
+        long numberExcludingLastDigit = n / 10;
+
+        return lastDigit + SumOfDigits(numberExcludingLastDigit);
+    }
+
+    static long SumOfDigitsOfFactorial(long n)
+    {
+        return SumOfDigits(Factorial(n));
+    }
+
+    static long SmallestNumberForSumOfDigitsOfFactorial(long i)
+    {
+        // Find smallest positive integer "n" such that: SumOfDigits(Factorial(n)) == i
+        long n = 1;
+
+        while(SumOfDigitsOfFactorial(n) != i)
+        {
+            n++;
+        }
+
+        return n;
+    }
+
+    static long SumOfMinimumNumbersForSumOfDigitsOfFactorial(int n)
+    {
+        var mappingsForSumOfDigitsOfFactorialToSmallestNumber = new Dictionary<long, long>();
+        long result = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            var sumOfDigitsOfFactorial = SumOfDigitsOfFactorial(i);
+
+            if (!mappingsForSumOfDigitsOfFactorialToSmallestNumber.TryGetValue(sumOfDigitsOfFactorial, out long smallestNumberForSumOfDigitsOfFactorial))
+            {
+                smallestNumberForSumOfDigitsOfFactorial = SmallestNumberForSumOfDigitsOfFactorial(i);
+                mappingsForSumOfDigitsOfFactorialToSmallestNumber.Add(sumOfDigitsOfFactorial, smallestNumberForSumOfDigitsOfFactorial);
+            }
+
+            result += SumOfDigits(smallestNumberForSumOfDigitsOfFactorial);
+        }
+
+        return result;
+    }
+
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int numberOfQueries = Int32.Parse(Console.ReadLine());
+
+        while (numberOfQueries-- > 0)
+        {
+            string[] digitModulo = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(digitModulo[0]);
+            long m = Convert.ToInt32(digitModulo[1]);
+
+            long result = SumOfMinimumNumbersForSumOfDigitsOfFactorial(n) % m;
+
+            Console.WriteLine(result);
+        }
+
+        Console.ReadKey();
+    }
+}
