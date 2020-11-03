1546a7199c3811c2f1194bdf37340a4dcf2a093d
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-11-03 09:47:02 +0530

    Today's challenge completed

--- LeetCode/20201103/InsertionSortList/Program.cs
+++ LeetCode/20201103/InsertionSortList/Program.cs
@@ -16,6 +16,16 @@ namespace InsertionSortList
             this.val = val;
             this.next = next;
         }
+
+        public override string ToString()
+        {
+            if (next == null)
+            {
+                return val.ToString();
+            }
+
+            return String.Format("{0}->{1}", val, next.ToString());
+        }
     }
 
     public class Solution
@@ -24,80 +34,116 @@ namespace InsertionSortList
         {
             if (head == null)
             {
-                // 0 or 1 element -> list is already sorted
-                return null;
+                // 0 elements - no need to sort
+                return head;
             }
 
-            ListNode sorted = head;
             ListNode current = head;
 
-            // How Insertion Sort works?
-            // It "splits" the list into three groups of nodes:
+            // How Insertion Sort works? It "splits" the list into three groups of nodes:
             // * sorted subset - starts as empty
             // * current node 
-            // * unsorted subset - empty in the end
+            // * unsorted subset - ends as empty
             // 
             // ** The subset of nodes lying before current node - is sorted
             // ** The subset of nodes lying after current node - yet to be sorted
-
+            //
             // The algorithm goes through every node:
-            // * compare current node with next node - this node lying after current item -> thus belongs to unsorted subset
-            // * if next node is greater than or equal to current node -> it is already sorted -> thus, mark it "current" and continue
-            // * if next node is less than current node then, need to find the correct end position for "next" node in sorted subset:
-            // ** backup next item somewhere into "temp" and "remove" it from list, by making current->next = current->next->next
+            // * compare current node with next node - "next" node lying after current item -> thus belongs to unsorted subset
+            // * if next node is greater than or equal to current node - it is already sorted -> thus, mark it "current" and continue insertion sort
+            // * if next node is less than current node - need to find correct end position for "next" node in sorted subset:
+            // ** save next node as "temp" - "delink" it from list, by making current->next = current->next->next
             // ** find right place for "temp":
             // *** start from beginning of "sorted" list
             // *** go forward until finding correct position for "temp"
             // *** insert "temp" there
             // * continue insertion sort
 
-            while (current != null)
+            while (current.next != null)
             {
-                var temp = current.next;
-
-                if (temp == null)
+                if (current.val <= current.next.val)
                 {
-                    break;
+                    // "current" is already in sorted position -> so, continue insertion sort
+                    current = current.next;
                 }
-
-                if (temp.val < current.val)
+                else
                 {
-                    // find right place for "temp"
-                    var sortedCurrent = sorted;
+                    var toInsert = current.next;
 
-                    if (sortedCurrent.val > temp.val)
+                    // delink "insert" so that it can be moved to its end position
+                    current.next = toInsert.next;
+
+                    // find right place for "insert"
+                    if (toInsert.val < head.val)
                     {
-                        sorted = temp;
-                        
+                        // "insert" becomes "head"
+                        toInsert.next = head;
+                        head = toInsert;
                     }
                     else
                     {
+                        var insertAt = head;
 
-                        
-                        var sortedNext = sortedCurrent.next;
-
-
-
-                        while (sortedCurrent != current)
+                        while ((insertAt.next != current) && (toInsert.val > insertAt.next.val))
                         {
-
-
-                            sortedCurrent = sortedCurrent.next;
+                            insertAt = insertAt.next;
                         }
+
+                        // insert at the found location
+                        toInsert.next = insertAt.next;
+                        insertAt.next = toInsert;
                     }
                 }
+            }            
 
-                current = current.next;
-            }
-            
-
-            return sorted;
+            return head;
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            var solution = new Solution();
+            ListNode node;
+
+            var result1 = solution.InsertionSortList(null); // null
+
+            var head2 = new ListNode(0);
+            var result2 = solution.InsertionSortList(head2); // 0
+
+            node = new ListNode(0);
+            var head3 = new ListNode(1, node);
+            var result3 = solution.InsertionSortList(head3); // 0->1
+
+            node = new ListNode(1);
+            var head4 = new ListNode(0, node);
+            var result4 = solution.InsertionSortList(head4); // 0->1
+
+            node = new ListNode(1);
+            node = new ListNode(0, node);
+            var head5 = new ListNode(2, node);
+            var result5 = solution.InsertionSortList(head5); // 0->1->2
+
+            node = new ListNode(2);
+            node = new ListNode(1, node);
+            var head6 = new ListNode(0, node);
+            var result6 = solution.InsertionSortList(head6); // 0->1->2
+
+            node = new ListNode(5);
+            node = new ListNode(4, node);
+            node = new ListNode(3, node);
+            node = new ListNode(2, node);
+            node = new ListNode(1, node);
+            var head7 = new ListNode(0, node);
+            var result7 = solution.InsertionSortList(head7); // 0->1->2->3->4->5
+
+            node = new ListNode(5);
+            node = new ListNode(2, node);
+            node = new ListNode(3, node);
+            node = new ListNode(1, node);
+            node = new ListNode(4, node);
+            var head8 = new ListNode(0, node);
+            var result8 = solution.InsertionSortList(head8); // 0->1->2->3->4->5
         }
     }
 }
