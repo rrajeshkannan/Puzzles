21dde81daee4630c0f05ee20cb3be73ceed6eb83
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-11-07 15:01:52 +0530

    Finished coding for the challenges

--- LeetCode/20201106/SmallestDivisorForThreshold/Program.cs
+++ LeetCode/20201106/SmallestDivisorForThreshold/Program.cs
@@ -6,49 +6,58 @@ using System.Threading.Tasks;
 
 namespace SmallestDivisorForThreshold
 {
+    // https://leetcode.com/explore/challenge/card/november-leetcoding-challenge/564/week-1-november-1st-november-7th/3521/
+    // Given an array of integers nums and an integer threshold, 
+    //   we will choose a positive integer divisor 
+    //   and divide all the array by it 
+    //   and sum the result of the division.
+    // 
+    // Find the smallest divisor such that the result mentioned above is less than or equal to threshold.
+    // 
+    // Each result of division is rounded to the nearest integer greater than or equal to that element.
+    //   (For example: 7/3 = 3 and 10/2 = 5)
+    //
+    // It is guaranteed that there will be an answer.
+
     public class Solution
     {
         public int SmallestDivisor(int[] nums, int threshold)
         {
-            //var divisors = Enumerable.Range(1, threshold).ToArray();
-            //binarySearch(nums, divisors, 0, threshold - 1, threshold);
-
-            return binarySearch(nums, 1, threshold, threshold);
+            return binarySearch(nums, 1, nums.Max(), threshold, Int32.MaxValue);
         }
 
-        static int binarySearch(int[] numbers, int left, int right, int search)
+        static int binarySearch(int[] numbers, int left, int right, int search, int lastChosenDivisor)
         {
-            if (right >= left)
+            if (right < left)
             {
-                int divisor = left + (right - left) / 2;
-
-                int sum = 0;
-                for (int i = 0; i < numbers.Length; i++)
-                {
-                    int roundedQuotient = (int)Math.Ceiling((double)numbers[i] / divisor);
-                    sum += roundedQuotient;
-                }
-
-                if (sum == search)
-                {
-                    return divisor;
-                }
-
-                if (sum > search)
-                {
-                    // search in right half
-                    return binarySearch(numbers, divisor + 1, right, search);
-                }
-                else
-                {
-                    // search in left half
-                    return binarySearch(numbers, left, divisor - 1, search);
-                }
+                // we cannot go further
+                return lastChosenDivisor;
             }
 
-            // We reach here when element is not present 
-            // in array 
-            return -1;
+            int divisor = left + (right - left) / 2;
+
+            int roundedSum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                roundedSum += (int)Math.Ceiling((double)numbers[i] / divisor);
+            }
+
+            if (roundedSum > search)
+            {
+                // if sum is bigger, it means chosen divisor is smaller
+                // try searching for divisor on the bigger numbers' side
+                // -> search in right half
+                return binarySearch(numbers, divisor + 1, right, search, lastChosenDivisor);
+            }
+            
+            // if sum is smaller, it can be a potential result to return
+            // -> still continue searching for possibly even smaller number
+            lastChosenDivisor = divisor;
+            
+            // if sum is smaller, it means chosen divisor is bigger
+            // try searching divisor on the smaller numbers' side
+            // -> search in left half
+            return binarySearch(numbers, left, divisor - 1, search, lastChosenDivisor);
         }
     }
 
@@ -65,6 +74,34 @@ namespace SmallestDivisorForThreshold
             var nums2 = new int[] { 2, 3, 5, 7, 11 };
             int threshold2 = 11;
             var result2 = solution.SmallestDivisor(nums2, threshold2); // 3
+
+            var nums3 = new int[] { 19 };
+            int threshold3 = 5;
+            var result3 = solution.SmallestDivisor(nums3, threshold3); // 4
+
+            var nums4 = new int[] { 2, 3, 5, 7, 11 };
+            int threshold4 = 9;
+            var result4 = solution.SmallestDivisor(nums4, threshold4); // 4
+
+            var nums5 = new int[] { 962551, 933661, 905225, 923035, 990560 };
+            int threshold5 = 10;
+            var result5 = solution.SmallestDivisor(nums5, threshold5); // 495280
+
+            var nums6 = new int[]
+            {
+                46480, 71852, 4544, 23598, 962, 66567, 66601, 90661, 30701, 30463, 
+                76184, 35590, 50634, 82516, 3847, 83498, 40938, 82092, 17753, 21195, 
+                3748, 94798, 77080, 49254, 24184, 81610, 80045, 69248, 10776, 45690, 
+                59496, 15406, 38198, 47381, 13353, 93106, 71420, 14775, 99118, 6866, 
+                62300, 57444, 3966, 91603, 56289, 26752, 16439, 96836, 80050, 14948, 
+                14487, 3034, 79113, 23445, 78123, 91204, 77022, 36837, 38978, 94389, 
+                77331, 523, 42947, 25830, 55630, 45936, 76823, 32614, 49959, 5111, 
+                74080, 59558, 79203, 93414, 11356, 87885, 50858, 4490, 11503, 35141, 
+                4446, 52051, 75511, 41767, 64622, 61572, 28298, 21584, 77878, 99083, 
+                47585, 75926, 84968, 12477, 86333, 55299, 99291, 47402, 82539, 19070
+            };
+            int threshold6 = 549;
+            var result6 = solution.SmallestDivisor(nums6, threshold6); // 10134
         }
     }
 }
---
+++ LeetCode/20201107/AddTwoLinkedLists/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoLinkedLists
+{
+    // https://leetcode.com/explore/challenge/card/november-leetcoding-challenge/564/week-1-november-1st-november-7th/3522/
+
+    // You are given two non-empty linked lists representing two non-negative integers. 
+    //   The most significant digit comes first and each of their nodes contain a single digit.
+    //
+    // Add the two numbers and return it as a linked list.
+    //
+    // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    //
+    // Follow up:
+    //   What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
+    // 
+    // Example:
+    //   Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+    //   Output: 7 -> 8 -> 0 -> 7
+
+    // Definition for singly-linked list.
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+        public override string ToString()
+        {
+            if (next == null)
+            {
+                return val.ToString();
+            }
+
+            return String.Format("{0}->{1}", val, next.ToString());
+        }
+    }
+    public class SolutionNotAccepted
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var number1 = GetNumberFromList(l1, out int digits1);
+            var number2 = GetNumberFromList(l2, out int digits2);
+            var digits = (digits1 > digits2) ? digits1 : digits2;
+
+            return GetListFromNumber(number1 + number2, digits);
+        }
+
+        public int GetNumberFromList(ListNode l, out int digits)
+        {
+            int number = 0;
+            digits = 0;
+
+            while (l != null)
+            {
+                number = (number * 10) + l.val;
+                l = l.next;
+                digits++;
+            }
+
+            return number;
+        }
+
+        public ListNode GetListFromNumber(int number, int digits)
+        {
+            ListNode head = null;
+
+            while (number != 0)
+            {
+                head = new ListNode(number % 10, head);
+                number /= 10;
+                digits--;
+            }
+
+            for (int i = digits; i > 0; i--)
+            {
+                head = new ListNode(0, head);
+            }
+
+            return head;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var number1 = GetStackFromList(l1);
+            var number2 = GetStackFromList(l2);
+
+            return AddTwoStacksOfNumbers(number1, number2);
+        }
+
+        public Stack<int> GetStackFromList(ListNode l)
+        {
+            var number = new Stack<int>();
+
+            while (l != null)
+            {
+                number.Push(l.val);
+                l = l.next;
+            }
+
+            return number;
+        }
+
+        public ListNode AddTwoStacksOfNumbers(Stack<int> number1, Stack<int> number2)
+        {
+            ListNode head = null;
+            var carry = 0;
+
+            while (number1.Any() && number2.Any())
+            {
+                var digit1 = number1.Pop();
+                var digit2 = number2.Pop();
+
+                var sum = digit1 + digit2 + carry;
+
+                carry = sum / 10;
+                head = new ListNode(sum % 10, head);
+            }
+
+            var number = number1.Any() ? number1 : number2.Any() ? number2 : new Stack<int>();
+
+            while(number.Any())
+            {
+                var digit = number.Pop();
+                var sum = digit + carry;
+
+                carry = sum / 10;
+                head = new ListNode(sum % 10, head);
+            }
+
+            if (carry > 0)
+            {
+                head = new ListNode(1, head);
+            }
+
+            return head;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var solutionNotAccepted = new SolutionNotAccepted();
+
+            var list11 = new ListNode(7, new ListNode(2, new ListNode(4, new ListNode(3))));
+            var list12 = new ListNode(5, new ListNode(6, new ListNode(4)));
+            var result1 = solutionNotAccepted.AddTwoNumbers(list11, list12);
+            var result12 = solution.AddTwoNumbers(list11, list12);
+
+            var list21 = new ListNode(0, new ListNode(0, new ListNode(0)));
+            var list22 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+            var result2 = solutionNotAccepted.AddTwoNumbers(list21, list22);
+            var result22 = solution.AddTwoNumbers(list21, list22);
+
+            var list31 = new ListNode(1, new ListNode(1, new ListNode(1, new ListNode(1))));
+            var list32 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+            var result3 = solutionNotAccepted.AddTwoNumbers(list31, list32);
+            var result32 = solution.AddTwoNumbers(list31, list32);
+
+            var list41 = new ListNode(0, new ListNode(0, new ListNode(0)));
+            var list42 = new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0))));
+            var result4 = solutionNotAccepted.AddTwoNumbers(list41, list42);
+            var result42 = solution.AddTwoNumbers(list41, list42);
+
+            // [3,9,9,9,9,9,9,9,9,9]
+            // [7]
+            var list51 = new ListNode(
+                3, new ListNode(
+                    9, new ListNode(
+                        9, new ListNode(
+                            9, new ListNode(
+                                9, new ListNode(
+                                    9, new ListNode(
+                                        9, new ListNode(
+                                            9, new ListNode(
+                                                9, new ListNode(
+                                                    9)
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                );
+            var list52 = new ListNode(7);
+            var result5 = solutionNotAccepted.AddTwoNumbers(list51, list52);
+            var result52 = solution.AddTwoNumbers(list51, list52);
+        }
+    }
+}
