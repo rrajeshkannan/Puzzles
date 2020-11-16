f1e8c2ad4c80bbe1ff1d9084ee241f8e599084ec
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-11-16 13:33:42 +0530

    Today's challenge and other hard problems solved

--- LeetCode/20201113/ShortestSubarrayWithSum/Program.cs
+++ LeetCode/20201113/ShortestSubarrayWithSum/Program.cs
@@ -7,6 +7,21 @@ using System.Threading.Tasks;
 
 namespace ShortestSubarrayWithSum
 {
+    // use technique of "prefix sum" in combination with "greedy + deque + monotonic stack"
+    // https://www.geeksforgeeks.org/maximum-subarray-sum-using-prefix-sum/
+    // https://en.wikipedia.org/wiki/Prefix_sum
+    // https://lifelongprogrammer.blogspot.com/2018/01/monotone-queue-stack-how-to-succeed-in-algorithms-interview.html
+    // https://dev.to/grekz/prefix-sum-suffix-sum-programming-tools-1eh5
+    // https://labuladong.gitbook.io/algo-en/ii.-data-structure/monotonicstack
+    // https://1e9.medium.com/monotonic-queue-notes-980a019d5793
+    // https://algorithmsandme.com/monotonic-queue/
+    // https://iq.opengenus.org/prefix-sum-array/
+    // https://www.programmersought.com/article/2116762024/
+    // https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/discuss/143726/C%2B%2BJavaPython-O(N)-Using-Deque
+    // https://www.google.com/search?q=prefix+sum+greedy+%2B+deque+%2B+monotonic+stack&rlz=1C1GCEA_enIN883IN883&oq=prefix+sum+greedy+%2B+deque+%2B+monotonic+stack&aqs=chrome..69i57.3935j0j7&sourceid=chrome&ie=UTF-8
+
+
+
     // https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
 
     // Return the length of the shortest, non-empty, contiguous subarray of A with sum at least K.
@@ -26,33 +41,54 @@ namespace ShortestSubarrayWithSum
     // -10 ^ 5 <= A[i] <= 10 ^ 5
     // 1 <= K <= 10 ^ 9
 
+    public class AnotherSolutionFromLeetCode
+    {
+        public int ShortestSubarray(int[] A, int K)
+        {
+            int res = Int32.MaxValue;
+            long[] prefixSum = new long[A.Length + 1];
+            for (int i = 1; i <= A.Length; i++)
+            {
+                prefixSum[i] = prefixSum[i - 1] + (long)A[i - 1];
+            }
+            LinkedList<int> q = new LinkedList<int>();
+            for (int i = 0; i <= A.Length; i++)
+            {
+                while (q.Count != 0 && prefixSum[q.First.Value] + K <= prefixSum[i])
+                {
+                    res = Math.Min(i - q.First.Value, res);
+                    q.RemoveFirst();
+                }
+                while (q.Count != 0 && prefixSum[q.Last.Value] >= prefixSum[i])
+                {
+                    q.RemoveLast();
+                }
+                q.AddLast(i);
+            }
+            return res == Int32.MaxValue ? -1 : res;
+        }
+    }
+
     public class Solution
     {
-        private int ProblemMax = 50000;
-
-        private void SkipNegativeOnFront(int[] A, int max, ref int begin)
+        private bool SubarraySumFirst(int[] A, int K, out int candidateArrayEnd, out int sum, out int subarrayBegin, out int subarrayEnd)
         {
-            for (; ((begin < max) && (A[begin] <= 0)); begin++)
-                ;
-        }
+            subarrayBegin = 0;
+            candidateArrayEnd = A.Length - 1;
 
-        private bool SubarraySumFirst(int[] A, int K, int length, int minSubarrayLength, ref int sum, ref int begin, ref int end)
-        {
-            for (; ((begin < end) && (A[begin] <= 0)); begin++)
+            // Skip negatives and zeros on front
+            while ((subarrayBegin <= candidateArrayEnd) && (A[subarrayBegin] <= 0))
+                subarrayBegin++;
+
+            // Skip negatives and zeros on rear
+            while ((candidateArrayEnd >= 0) && (A[candidateArrayEnd] <= 0))
+                candidateArrayEnd--;
+
+            sum = 0;
+
+            for (subarrayEnd = subarrayBegin; subarrayEnd <= candidateArrayEnd; subarrayEnd++)
             {
-                sum -= A[begin];
-            }
-
-            int max = begin + minSubarrayLength;
-
-            if (max > length)
-            {
-                max = length;
-            }
-
-            for (end = begin; end < max; end++)
-            {
-                sum += A[end];
+                sum += A[subarrayEnd];
 
                 if (sum >= K)
                 {
@@ -63,31 +99,38 @@ namespace ShortestSubarrayWithSum
             return false;
         }
 
-        private bool SubarraySum (int[] A, int K, int length, int minSubarrayLength, ref int sum, ref int begin, ref int end)
+        private bool SubarraySumNext (int[] A, int K, int candidateArrayEnd, int minSubarrayLength, ref int sum, ref int begin, ref int end)
         {
-            for (; ((begin < end) && (A[begin] <= 0)); begin++)
+            // Skip negatives and zeros
+            while ((begin <= end) && (A[begin] <= 0))
+                sum -= A[begin++];
+
+            // find possibility of preponing "end" by coming from rear
+            do
             {
-                sum -= A[begin];
+                while ((end > begin) && (sum > K))
+                    sum -= A[end--];
+
+                while ((end > begin) && (A[end] <= 0))
+                    sum -= A[end--];
+            } while ((end > begin) && (sum > K));
+
+            if (sum == K)
+                return true;
+
+            int maxSubarrayIndex = begin + minSubarrayLength - 1;
+
+            if (maxSubarrayIndex > candidateArrayEnd)
+            {
+                maxSubarrayIndex = candidateArrayEnd;
             }
 
-            int max = begin + minSubarrayLength;
-
-            if (max > length)
+            while ((end < maxSubarrayIndex) && (sum < K))
             {
-                max = length;
+                sum += A[++end];
             }
 
-            for (end = begin; end < max; end++)
-            {
-                sum += A[end];
-
-                if (sum >= K)
-                {
-                    return true;
-                }
-            }
-
-            return false;
+            return (sum >= K);
         }
 
         public int ShortestSubarray(int[] A, int K)
@@ -97,22 +140,21 @@ namespace ShortestSubarrayWithSum
                 return -1;
             }
 
-            int length = A.Length;
-            var minSubarrayLength = ProblemMax + 1;
-            var found = false;
+            var found = SubarraySumFirst(A, K, out int candidateArrayEnd, out int sum, out int subarrayBegin, out int subarrayEnd);
 
-            int subarrayBegin = 0;
-            SkipNegativeOnFront(A, length, ref subarrayBegin);
-
-            int sum = 0;
-            int subarrayEnd = length - 1;
-
-            for (; subarrayBegin < length; )
+            if (!found)
             {
-                if (SubarraySum(A, K, length, minSubarrayLength, ref sum, ref subarrayBegin, ref subarrayEnd))
-                {
-                    found = true;
+                return -1;
+            }
 
+            var minSubarrayLength = subarrayEnd - subarrayBegin + 1;
+
+            for (; (subarrayBegin <= candidateArrayEnd) && (subarrayEnd <= candidateArrayEnd);)
+            {
+                sum -= A[subarrayBegin++];
+
+                if (SubarraySumNext(A, K, candidateArrayEnd, minSubarrayLength, ref sum, ref subarrayBegin, ref subarrayEnd))
+                {
                     minSubarrayLength = subarrayEnd - subarrayBegin + 1;
 
                     if (minSubarrayLength == 1)
@@ -120,19 +162,10 @@ namespace ShortestSubarrayWithSum
                         // Nothing can be lesser than 1
                         return 1;
                     }
-
-                    sum -= A[subarrayBegin];
-                    subarrayBegin++;
-                }
-                else
-                {
-                    // could not find a subarray between subarrayBegin and subarrayEnd
-                    // so, continue outside this subarray
-                    subarrayBegin = subarrayEnd + 1;
                 }
             }
 
-            return found ? minSubarrayLength : -1;
+            return minSubarrayLength;
         }
     }
 
@@ -141,6 +174,7 @@ namespace ShortestSubarrayWithSum
         static void Main()
         {
             var solution = new Solution();
+            //var solution = new AnotherSolutionFromLeetCode();
 
             var a1 = new int[] { 1 };
             var result1 = solution.ShortestSubarray(a1, 1); // 1
@@ -155,33 +189,57 @@ namespace ShortestSubarrayWithSum
             var result4 = solution.ShortestSubarray(a4, 3); // 2
 
             var a5 = TestData.Data1();
-            Stopwatch watch = Stopwatch.StartNew();
+            Stopwatch watch5 = Stopwatch.StartNew();
             var result5 = solution.ShortestSubarray(a5, 5006414); // 19678
-            watch.Stop();
-
+            watch5.Stop();
             Console.WriteLine(result5);
-
-            var time = watch.ElapsedMilliseconds;
-            Console.WriteLine(time);
+            Console.WriteLine(watch5.ElapsedMilliseconds);
 
             var a6 = new int[] { 17, 85, 93, -45, -21 };
             var result6 = solution.ShortestSubarray(a6, 150); // 2
 
-            // 2,20263,20262
-            // 4,20264,20261
-            // 5,20265,20261
-            // 7,20266,20260
-            // 9,20266,20258
-            // 10,20267,20258
-            // 14,20266,
+            var a7 = new int[] { -34, 37, 51, 3, -12, -50, 51, 100, -47, 99, 34, 14, -13, 89, 31, -14, -44, 23, -38, 6 };
+            var result7 = solution.ShortestSubarray(a7, 151); // 2
 
+            // 37+51+3-12-50+51+100 = 180 => 7
 
+            // 51+3-12-50+51+100 = 143 => 6
+            // 51+3-12-50+51+100-47 = 96 => xx
 
-            // 3416 - Debug
-            // 781 - Release
+            // 3-12-50+51+100-47 = 45 => 6
+            // 3-12-50+51+100-47+99 = 144 => xx
 
-            // 3403 - Debug
-            // 774 - Release
+            // -12-50+51+100-47+99 = 141 => 6
+            // 51+100-47+99 = 203 => 4
+            // 51+100-47 = 104 => xx
+            // 51+100-47+99 = 203 => 4
+
+            var a8 = new int[] { -36, 10, -28, -42, 17, 83, 87, 79, 51, -26, 33, 53, 63, 61, 76, 34, 57, 68, 1, -30 };
+            var result8 = solution.ShortestSubarray(a8, 484); // 9
+
+            // 10-28-42+17+83+87+79+51-26+33+53+63+61+76 = 517 => 14
+            // -28-42+17+83+87+79+51-26+33+53+63+61+76 = 507 => 13
+            // -42+17+83+87+79+51-26+33+53+63+61+76 = 535 => 12
+            // 17+83+87+79+51-26+33+53+63+61+76 = 577 => 11
+            // 17+83+87+79+51-26+33+53+63+61 = 501 => 10
+            // 17+83+87+79+51-26+33+53+63 = 440 => 9
+            // 17+83+87+79+51-26+33+53+63+61 = 501 => 10
+
+            // 83+87+79+51-26+33+53+63+61 = 484 => 9
+
+            // 33+53+63+61+76+34+57+68+1 = 446 => xx
+            // 51-26+33+53+63+61+76+34+57+68+1 = 471 => xx
+
+            var a9 = TestData.Data2();
+            Stopwatch watch9 = Stopwatch.StartNew();
+            var result9 = solution.ShortestSubarray(a9, 10723661); // Expected: 42622, Output: 42626
+            watch9.Stop();
+            Console.WriteLine(result9);
+            Console.WriteLine(watch9.ElapsedMilliseconds);
+
+            var a10 = new int[] { 58, -27, -11, 63, 90, 83, 61, -44, -39, 30 };
+            var result10 = solution.ShortestSubarray(a10, 61); // 1
+
             Console.ReadKey();
         }
     }
---
+++ LeetCode/20201116/BST_Range_Sum/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_Range_Sum
+{
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //public class Solution
+    //{
+    //public int RangeSumBST(TreeNode root, int low, int high)
+    //{
+    //    if (root == null)
+    //    {
+    //        return 0;
+    //    }
+
+    //    var sum = ((root.val >= low) && (root.val <= high)) ? root.val : 0;
+
+    //    return sum + RangeSumBST(root.left, low, high) + RangeSumBST(root.right, low, high);
+    //}
+    //}
+
+    public class Solution
+    {
+        private int _low;
+        private int _high;
+        private int _sum;
+
+        public int RangeSumBST(TreeNode root, int low, int high)
+        {
+            _low = low;
+            _high = high;
+            _sum = 0;
+            void Traverse(TreeNode node)
+            {
+                if (node == null) return;
+
+                if (low <= node.val && node.val <= _high)
+                {
+                    _sum += node.val;
+                    Traverse(node.right);
+                    Traverse(node.left);
+                }
+                else if (node.val >= _high)
+                {
+                    Traverse(node.left);
+                }
+                else if (node.val <= _low)
+                {
+                    Traverse(node.right);
+                }
+            }
+
+            Traverse(root);
+            return _sum;
+        }
+    }
+
+    //public class Solution
+    //{
+    //    int sum = 0;
+    //    public int RangeSumBST(TreeNode root, int L, int R)
+    //    {
+    //        InOrder(root, L, R);
+    //        return sum;
+    //    }
+
+    //    private void InOrder(TreeNode node, int L, int R)
+    //    {
+    //        if (node == null)
+    //            return;
+
+    //        if (node.val >= L && node.val <= R)
+    //        {
+    //            sum += node.val;
+    //            if (node.val <= R)
+    //                InOrder(node.left, L, R);
+    //            if (node.val >= L)
+    //                InOrder(node.right, L, R);
+
+    //        }
+    //        else if (node.val < L)
+    //        {
+    //            InOrder(node.right, L, R);
+    //        }
+    //        else if (node.val > R)
+    //        {
+    //            InOrder(node.left, L, R);
+    //        }
+    //    }
+    //}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var root1 = new TreeNode(
+                10,
+                new TreeNode(
+                    5,
+                    new TreeNode(3),
+                    new TreeNode(7)),
+                new TreeNode(
+                    15,
+                    null,
+                    new TreeNode(18)));
+            var result1 = solution.RangeSumBST(root1, 7, 15); // 32
+
+            var root2 = new TreeNode(
+                10,
+                new TreeNode(
+                    5,
+                    new TreeNode(
+                        3,
+                        new TreeNode(1)),
+                    new TreeNode(
+                        7,
+                        new TreeNode(6))),
+                new TreeNode(
+                    15,
+                    new TreeNode(13),
+                    new TreeNode(18)));
+            var result2 = solution.RangeSumBST(root2, 6, 10); // 23
+        }
+    }
+}
