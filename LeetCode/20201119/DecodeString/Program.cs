0ffb2d498ab3bfdd49c238eaeceabdbbb5d9bf7f
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-11-22 11:46:30 +0530

    unfinished solutions

---
+++ LeetCode/20201119/DecodeString/Program.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DecodeString
+{
+    public abstract class DecodeState
+    {
+        protected DecodeContext Context { get; }
+
+        public DecodeState(DecodeContext context) => Context = context;
+
+        public abstract String Decode(string input, ref int index);
+    }
+
+    public class Begin : DecodeState
+    {
+        public Begin(DecodeContext context) : base(context)
+        {
+        }
+
+        public override String Decode(string input, ref int index)
+        {
+            if (index >= input.Length)
+            {
+                return String.Empty;
+            }
+
+            var character = input[index];
+
+            if (character >= 'a' && character <= 'z')
+            {
+
+            }
+        }
+    }
+
+    public class RepeatCount : DecodeState
+    {
+        public RepeatCount(DecodeContext context) : base(context)
+        {
+        }
+
+        public override String Decode(string input, ref int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Text : DecodeState
+    {
+        public Text(DecodeContext context) : base(context)
+        {
+        }
+
+        public override String Decode(string input, ref int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DecodeContext
+    {
+        public DecodeState Begin { get; }
+        public DecodeState RepeatCount { get; }
+        public DecodeState RepeatText { get; }
+
+        public DecodeState State { get; set; }
+
+        public DecodeContext()
+        {
+            Begin = new Begin(this);
+            RepeatCount = new RepeatCount(this);
+            RepeatText = new Text(this);
+
+            State = Begin;
+        }
+
+        public string Decode (string input)
+        {
+            if (String.IsNullOrEmpty(input))
+            {
+                return String.Empty;
+            }
+
+            int index = 0;
+            return State.Decode(input, ref index);
+        }
+    }
+
+    public class Solution
+    {
+        private DecodeContext _context = new DecodeContext();
+
+        public string DecodeString(string input)
+        {
+            var result = "";
+
+            var countStarted = false;
+            var repeatCount = 0;
+
+            var substring = "";
+
+            foreach (char character in input)
+            {
+                if (char.IsLetter(character))
+                {
+                    substring += character;
+                }
+                else if (char.IsDigit(character))
+                {
+                    countStarted = true;
+
+                    var digit = Convert.ToInt32(character);
+                    repeatCount = repeatCount * 10 + digit;
+                }
+                else if (character == '[')
+                {
+                    substring = "";
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public string DecodeString(string input, ref int index)
+        {
+            for (; index < input.Length; index++)
+            {
+                var character = input[index];
+
+                if (char.IsLetter(character))
+                {
+                }
+            }
+
+            return String.Empty;
+        }
+
+        public string ExtractSubstring(string input, ref int index)
+        {
+            var substring = "";
+
+            for (; index < input.Length; index++)
+            {
+                var character = input[index];
+
+                if (!char.IsLetter(character))
+                    break;
+
+                substring += character;
+            }
+            
+            return String.Empty;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            solution.DecodeString("");
+
+            //var result1 = solution.DecodeString("3[a]2[bc]");
+            //var result2 = solution.DecodeString("3[a2[c]]");
+            //var result3 = solution.DecodeString("2[abc]3[cd]ef");
+            //var result4 = solution.DecodeString("abc3[cd]xyz");
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+public class dummy
+{
+
+    public void DecodeString()
+    {
+        var input = @"add(mul(a,add(b,c)),d) + e - sub(f,g)";
+        var pattern =
+            @"\(                    # Match (" +
+            @"(" +
+            @"        [^()]+            # all chars except ()" +
+            @"        | (?<Level>\()    # or if ( then Level += 1" +
+            @"        | (?<-Level>\))   # or if ) then Level -= 1" +
+            @"    )+                    # Repeat (to go from inside to outside)" +
+            @"    (?(Level)(?!))        # zero-width negative lookahead assertion" +
+            @"    \)                    # Match )";
+
+
+        Match m = Regex.Match(input, pattern,
+            RegexOptions.IgnorePatternWhitespace);
+        if (m.Success == true)
+        {
+            Console.WriteLine("Input: \"{0}\" \nMatch: \"{1}\"", input, m);
+            int grpCtr = 0;
+            foreach (Group grp in m.Groups)
+            {
+                Console.WriteLine("   Group {0}: {1}", grpCtr, grp.Value);
+                grpCtr++;
+                int capCtr = 0;
+                foreach (Capture cap in grp.Captures)
+                {
+                    Console.WriteLine("      Capture {0}: {1}", capCtr, cap.Value);
+                    capCtr++;
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Match failed.");
+        }
+
+
+        //string pattern = @"[0-9]*";
+
+        //string[] substrings = Regex.Split(input, pattern);    // Split on numbers
+        //foreach (string match in substrings)
+        //{
+        //    Console.WriteLine("'{0}'", match);
+        //}
+    }
+
+
+
+
+    public string DecodeString2(string input)
+    {
+        input = @"add(mul(a,add(b,c)),d) + e - sub(f,g)";
+        var pattern =
+            @"\(                    # Match (" +
+            @"(" +
+            @"        [^()]+            # all chars except ()" +
+            @"        | (?<Level>\()    # or if ( then Level += 1" +
+            @"        | (?<-Level>\))   # or if ) then Level -= 1" +
+            @"    )+                    # Repeat (to go from inside to outside)" +
+            @"    (?(Level)(?!))        # zero-width negative lookahead assertion" +
+            @"    \)                    # Match )";
+
+        var regex = new Regex(pattern,
+            RegexOptions.IgnorePatternWhitespace);
+
+        foreach (Match c in regex.Matches(input))
+        {
+            Console.WriteLine(c.Value);
+            //Console.WriteLine(c.Value.Trim('(', ')'));
+        }
+
+
+        //string pattern = @"[0-9]*";
+
+        //string[] substrings = Regex.Split(input, pattern);    // Split on numbers
+        //foreach (string match in substrings)
+        //{
+        //    Console.WriteLine("'{0}'", match);
+        //}
+
+        return String.Empty;
+    }
+
+    public string DecodeString1(string input)
+    {
+        //var pattern1 = @"([0-9*?])";
+        //var pattern2 = @"\[(.*?)\]";
+
+
+        //string pattern = "^[^<>]*" +
+        //           "(" +
+        //           "((?'Open'<)[^<>]*)+" +
+        //           "((?'Close-Open'>)[^<>]*)+" +
+        //           ")*" +
+        //           "(?(Open)(?!))$";
+        //string input = "<abc><mno<xyz>>";
+
+        string pattern = @"^[^0-9]*" + @"(?'Count'\d+)"; // @"(\d+)"; 
+
+        Match m = Regex.Match(input, pattern);
+        if (m.Success == true)
+        {
+            Console.WriteLine("Input: \"{0}\" \nMatch: \"{1}\"", input, m);
+            int grpCtr = 0;
+            foreach (Group grp in m.Groups)
+            {
+                Console.WriteLine("   Group {0}: {1}", grpCtr, grp.Value);
+                grpCtr++;
+                int capCtr = 0;
+                foreach (Capture cap in grp.Captures)
+                {
+                    Console.WriteLine("      Capture {0}: {1}", capCtr, cap.Value);
+                    capCtr++;
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Match failed.");
+        }
+
+        Console.WriteLine();
+
+
+        return String.Empty;
+    }
+}
---
+++ LeetCode/20201119/MergeIntervals/Program.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeIntervals
+{
+    // https://leetcode.com/explore/challenge/card/november-leetcoding-challenge/566/week-3-november-15th-november-21st/3535/
+
+    public class Solution
+    {
+        public int[][] Merge(int[][] intervals)
+        {
+            //var mergedIntervals = intervals
+            //    .SelectMany(interval => new int[] { interval[0], interval[1] })
+            //    .OrderBy(interval => interval)
+            //    .Distinct();
+
+            var flatIntervals = intervals
+                .SelectMany(interval => new int[] { interval[0], interval[1] })
+                .ToArray();
+
+            var flatOrderedIntervals = flatIntervals
+                .OrderBy(interval => interval)
+                .ToArray();
+
+            var mergedIntervals = flatIntervals
+                .Distinct()
+                .ToArray();
+
+            var beginIntervals = mergedIntervals.Where((interval, index) => index % 2 == 0);
+            var endIntervals = mergedIntervals.Where((interval, index) => index % 2 == 1);
+
+            var result = beginIntervals
+                .Zip(endIntervals, (beginInterval, endInterval) => new int[] { beginInterval, endInterval })
+                .ToArray();
+
+            return result;
+
+            //return beginIntervals
+            //    .Zip(endIntervals, (beginInterval, endInterval) => new int[] { beginInterval, endInterval })
+            //    .ToArray();
+        }
+
+        public class IntervalsComparer : IComparer<int[]>
+        {
+            public int Compare(int[] x, int[] y)
+            {
+                if (x[0] < y[0])
+                    return -1;
+                
+                if (x[0] > y[0])
+                    return 1;
+
+                if (x[1] < y[1])
+                    return -1;
+
+                if (x[1] > y[1])
+                    return 1;
+
+                return 0;
+            }
+        }
+
+        public int[][] Merge1(int[][] intervals)
+        {
+            int Merge(int front, int rear)
+            {
+                if (rear >= intervals.Length)
+                {
+                    return front;
+                }
+
+                var merged = false;
+
+                if (intervals[front][0] >= intervals[rear][0])
+                {
+                    intervals[front][0] = intervals[rear][0];
+
+                    if (intervals[front][1] < intervals[rear][1])
+                    {
+                        intervals[front][1] = intervals[rear][1];
+                    }
+
+                    merged = true;
+                }
+                else if (intervals[front][1] >= intervals[rear][0])
+                {
+                    if (intervals[front][1] < intervals[rear][1])
+                    {
+                        intervals[front][1] = intervals[rear][1];
+                    }
+
+                    merged = true;
+                }
+                else if (intervals[front][1] > intervals[rear][1])
+                {
+                    return front;
+                }
+                
+                if (merged)
+                {
+                    if (front > 0)
+                    {
+                        // check opportunities for merging of intervals that are already resolved before
+                        front = Merge(front - 1, front);
+                    }
+
+                    // else, front maintains its position due to the merge
+                }
+                else
+                {
+                    // no merge happened, so, front moves ahead
+                    front++;
+
+                    // preserve current interval into results at new front
+                    intervals[front][0] = intervals[rear][0];
+                    intervals[front][1] = intervals[rear][1];
+                }
+
+                // rear moves ahead with next interval
+                return Merge(front, rear + 1);
+            }
+
+            intervals = intervals.OrderBy(interval => interval, new IntervalsComparer()).ToArray();
+
+            var lastMergedPosition = Merge(0, 1);
+            return intervals.Take(lastMergedPosition + 1).ToArray();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var intervals1 = new int[][] {
+                new int[] { 1, 3 },
+                new int[] { 2, 6 },
+                new int[] { 8, 10 },
+                new int[] { 15, 18 }
+            };
+            var result1 = solution.Merge(intervals1); // [[1,6],[8,10],[15,18]]
+
+            var intervals9 = new int[][] {
+                new int[] { 1, 2 },
+                new int[] { 3, 6 },
+                new int[] { 8, 10 },
+                new int[] { 15, 18 }
+            };
+            var result9 = solution.Merge(intervals9); // [[1,2],[3,6],[8,10],[15,18]]
+
+            var intervals10 = new int[][]
+            {
+                new int[] { 1, 4 }
+            };
+            var result10 = solution.Merge(intervals10); // [[1,4]]
+
+            var intervals2 = new int[][]
+            {
+                new int[] { 1, 4 },
+                new int[] { 4, 5 }
+            };
+            var result2 = solution.Merge(intervals2); // [[1,5]]
+
+            var intervals3 = new int[][]
+            {
+                new int[] { 0, 0 },
+                new int[] { 0, 0 }
+            };
+            var result3 = solution.Merge(intervals3); // [[0,0]]
+
+            var intervals4 = new int[][] {
+                new int[] { 1, 10 },
+                new int[] { 2, 3 },
+                new int[] { 8, 10 },
+                new int[] { 15, 18 }
+            };
+            var result4 = solution.Merge(intervals4); // [[1,10][15,18]]
+
+            var intervals5 = new int[][] {
+                new int[] { 1, 11 },
+                new int[] { 2, 3 },
+                new int[] { 8, 10 },
+                new int[] { 15, 18 }
+            };
+            var result5 = solution.Merge(intervals5); // [[1,11][15,18]]
+
+            var intervals6 = new int[][] {
+                new int[] { 1, 15 },
+                new int[] { 2, 3 },
+                new int[] { 8, 10 },
+                new int[] { 15, 18 }
+            };
+            var result6 = solution.Merge(intervals6); // [[1,18]]
+
+            var intervals7 = new int[][]
+            {
+                new int[] { 1, 4 },
+                new int[] { 0, 4 }
+            };
+            var result7 = solution.Merge(intervals7); // [[0,4]]
+
+            var intervals8 = new int[][]
+            {
+                new int[] { 1, 2 },
+                new int[] { 3, 4 },
+                new int[] { 0, 5 }
+            };
+            var result8 = solution.Merge(intervals8); // [[0,5]]
+
+            var intervals11 = new int[][]
+            {
+                new int[] { 1, 4 },
+                new int[] { 0, 0 }
+            };
+            var result11 = solution.Merge(intervals11); // [[0,0],[1,4]]
+
+            var intervals12 = new int[][]
+            {
+                new int[] { 1, 4 },
+                new int[] { 0, 1 }
+            };
+            var result12 = solution.Merge(intervals12); // [[0,4]]
+
+            var intervals13 = new int[][]
+            {
+                new int[] {2, 3},
+                new int[] {2, 2},
+                new int[] {3, 3},
+                new int[] {1, 3},
+                new int[] {5, 7},
+                new int[] {2, 2},
+                new int[] {4, 6}
+            };
+            var result13 = solution.Merge(intervals13); // [[1,3],[4,7]]
+        }
+    }
+}
