d6e2b7b560b5f7246bfa236693e6e607395c3111
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-11-22 10:23:19 +0530

    Weekly contest

---
+++ LeetCode/20201122/AreTwoStringsEqual/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreTwoStringsEqual
+{
+    // https://leetcode.com/contest/weekly-contest-216/problems/check-if-two-string-arrays-are-equivalent/
+
+    class Program
+    {
+        public class Solution
+        {
+            public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+            {
+                var length1 = 0;
+
+                for (int i = 0; i < word1.Length; i++)
+                {
+                    length1 += word1[i].Length;
+                }
+
+                var length2 = 0;
+
+                for (int i = 0; i < word2.Length; i++)
+                {
+                    length2 += word2[i].Length;
+                }
+
+                if (length1 != length2)
+                {
+                    return false;
+                }
+
+                var s1 = "";
+
+                for (int i = 0; i < word1.Length; i++)
+                {
+                    s1 += word1[i];
+                }
+
+                var s2 = "";
+
+                for (int i = 0; i < word2.Length; i++)
+                {
+                    s2 += word2[i];
+                }
+
+                return (s1 == s2);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var word11 = "ab";
+            var word12 = "c";
+            var word21 = "a";
+            var word22 = "bc";
+
+            var result1 = solution.ArrayStringsAreEqual(
+                new string[] { word11, word12 },
+                new string[] { word21, word22 }); // true
+
+
+        }
+    }
+}
---
+++ LeetCode/20201122/SmallestStringForNumericValue/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallestStringForNumericValue
+{
+    // https://leetcode.com/contest/weekly-contest-216/problems/smallest-string-with-a-given-numeric-value/
+
+
+
+    class Program
+    {
+        public class Solution
+        {
+            public string GetSmallestString(int n, int k)
+            {
+                var result = String.Empty;
+
+                if (n <= 0)
+                {
+                    return result;
+                }
+
+                var alphabets = Enumerable.Range('a', 'z' - 'a' + 1)
+                    .Select(c => (char)c)
+                    .ToList();
+
+                var chosen = alphabets.Count;
+                var repeat = k / chosen;
+                var nBalance = n - repeat;
+
+                if (repeat > 0)
+                {
+                    var balance = k % chosen;
+
+                    if (balance < n - repeat)
+                    {
+                        repeat--;
+                    }
+
+                    result = new string(Enumerable.Repeat('z', repeat)
+                        .Select(c => (char)c)
+                        .ToArray());
+
+                    k = k - (chosen * repeat);
+                    n = n - repeat;
+                }
+
+                if (n > 0)
+                {
+                    chosen = k - (n - 1);
+
+                    result = alphabets[chosen - 1] + result;
+                    k -= chosen;
+                    n--;
+
+                    var prefix = new string(Enumerable.Repeat('a', n)
+                            .Select(c => (char)c)
+                            .ToArray());
+
+                    result = prefix + result;
+                }
+
+                return result;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var result1 = solution.GetSmallestString(3, 27); // "aay"
+            var result2 = solution.GetSmallestString(5, 73); // "aaszz"
+            var result3 = solution.GetSmallestString(2, 2); // "aa"
+            var result4 = solution.GetSmallestString(2, 3); // "ab"
+            var result5 = solution.GetSmallestString(2, 5); // "ad"
+            var result6 = solution.GetSmallestString(3, 7); // "aaf"
+
+            var watch = Stopwatch.StartNew();
+            var tesolution = new TimeExceededSolution();
+            var result7 = tesolution.GetSmallestString(74657, 743771)
+                .TakeWhile(character => character == 'a')
+                .Count();
+            Console.WriteLine(result7);
+            Console.WriteLine(watch.ElapsedMilliseconds);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class TimeExceededSolution
+    {
+        public string GetSmallestString(int n, int k)
+        {
+            var alphabets = Enumerable.Range('a', 'z' - 'a' + 1).
+                 Select(c => (char)c)
+                 .ToList();
+
+            var result = String.Empty;
+
+            while (n > 0)
+            {
+                var chosen = alphabets.Count;
+                var kBalance = k - chosen;
+                var nBalance = n - 1;
+
+                if (kBalance >= nBalance)
+                {
+                    result = alphabets[chosen - 1] + result;
+
+                    k -= chosen;
+                    n--;
+                }
+                else
+                {
+                    chosen = k - (n - 1);
+
+                    result = alphabets[chosen - 1] + result;
+                    k -= chosen;
+                    n--;
+
+                    while (n > 0)
+                    {
+                        chosen = 1;
+                        result = alphabets[chosen - 1] + result;
+                        k -= chosen;
+                        n--;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
