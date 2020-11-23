8091cb6905123476d1cf71fd3aa4fd1ab40bab81
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-11-23 19:35:19 +0530

    Yesterday's challenge completed and weekly contest problem unfinished yet

--- LeetCode/20201122/SmallestStringForNumericValue/Program.cs
+++ LeetCode/20201122/SmallestStringForNumericValue/Program.cs
@@ -17,12 +17,12 @@ namespace SmallestStringForNumericValue
         {
             public string GetSmallestString(int n, int k)
             {
-                var result = String.Empty;
-
                 if (n <= 0)
                 {
-                    return result;
+                    return String.Empty;
                 }
+
+                var result = new StringBuilder();
 
                 var alphabets = Enumerable.Range('a', 'z' - 'a' + 1)
                     .Select(c => (char)c)
@@ -30,41 +30,50 @@ namespace SmallestStringForNumericValue
 
                 var chosen = alphabets.Count;
                 var repeat = k / chosen;
-                var nBalance = n - repeat;
 
                 if (repeat > 0)
                 {
-                    var balance = k % chosen;
+                    var nBalance = n - repeat;
 
-                    if (balance < n - repeat)
+                    if (nBalance > 0)
                     {
-                        repeat--;
+                        // how many number of 'z' from "repeat" can be donated by replacing 'z' with 'a' to reach required 'n'?
+                        // (26 - 1) => 26 represents 'z', 1 represents 'a'
+                        //   for 1 'z', 26 'a' can come in => 26 letters replace 1 letter
+                        //   meaning a net addition of 25 letters coming in => 1 'z' replaced by 1 'a' + 25 'a'
+                        var donateFromRepeat = nBalance / (chosen - 1);
+                        
+                        //var donatedLeftovers = nBalance % (chosen - 1);
+
+                        //if (donatedLeftovers > 0)
+                        //{
+                        //    donateFromRepeat++;
+                        //}
+
+                        repeat -= donateFromRepeat;
                     }
 
-                    result = new string(Enumerable.Repeat('z', repeat)
-                        .Select(c => (char)c)
-                        .ToArray());
-
-                    k = k - (chosen * repeat);
-                    n = n - repeat;
+                    result.Insert(0, new string('z', repeat));
+                    k -= chosen * repeat;
+                    n -= repeat;
                 }
 
                 if (n > 0)
                 {
                     chosen = k - (n - 1);
 
-                    result = alphabets[chosen - 1] + result;
-                    k -= chosen;
-                    n--;
+                    if (chosen > 0)
+                    {
+                        result.Insert(0, alphabets[chosen - 1]);
 
-                    var prefix = new string(Enumerable.Repeat('a', n)
-                            .Select(c => (char)c)
-                            .ToArray());
+                        k -= chosen;
+                        n--;
+                    }
 
-                    result = prefix + result;
+                    result.Insert(0, new string('a', n));
                 }
 
-                return result;
+                return result.ToString();
             }
         }
 
@@ -73,19 +82,75 @@ namespace SmallestStringForNumericValue
             var solution = new Solution();
 
             var result1 = solution.GetSmallestString(3, 27); // "aay"
-            var result2 = solution.GetSmallestString(5, 73); // "aaszz"
-            var result3 = solution.GetSmallestString(2, 2); // "aa"
-            var result4 = solution.GetSmallestString(2, 3); // "ab"
-            var result5 = solution.GetSmallestString(2, 5); // "ad"
-            var result6 = solution.GetSmallestString(3, 7); // "aaf"
+            //var result2 = solution.GetSmallestString(5, 73); // "aaszz"
+            //var result3 = solution.GetSmallestString(2, 2); // "aa"
+            //var result4 = solution.GetSmallestString(2, 3); // "ab"
+            //var result5 = solution.GetSmallestString(2, 5); // "ad"
+            //var result6 = solution.GetSmallestString(3, 7); // "aae"
+            //var result9 = solution.GetSmallestString(2, 52); // "zz"
+            //var result10 = solution.GetSmallestString(2, 51); // "yz"
+            //var result11 = solution.GetSmallestString(80, 576);
+            //// Expected:
+            //// "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaavzzzzzzzzzzzzzzzzzzz"
+            //// Actual:
+            //// "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaazzzzzzzzzzzzzzzzzzzz"
 
-            var watch = Stopwatch.StartNew();
-            var tesolution = new TimeExceededSolution();
-            var result7 = tesolution.GetSmallestString(74657, 743771)
-                .TakeWhile(character => character == 'a')
-                .Count();
-            Console.WriteLine(result7);
-            Console.WriteLine(watch.ElapsedMilliseconds);
+            //var watch7 = Stopwatch.StartNew();
+            //var result7 = solution.GetSmallestString(74657, 743771);
+            //// 'a' - 47892
+            //// 'o' - 1
+            //// 'z' - 26764
+            //watch7.Stop();
+            //Console.WriteLine(watch7.ElapsedMilliseconds); // 5
+
+            //var result7Groups = result7
+            //    .GroupBy(character => character)
+            //    .Select(group => new { character = group.Key, count = group.Count() })
+            //    .ToList();
+
+            //for (int i = 0; i < result7Groups.Count; i++)
+            //{
+            //    Console.WriteLine("Group-character: {0}, Group-count: {1}", result7Groups[i].character, result7Groups[i].count);
+            //}
+
+            var tesolution = new Solution();
+            var watch101 = Stopwatch.StartNew();
+            var result101 = tesolution.GetSmallestString(74657, 743771);
+            // a - 47892
+            // o - 1
+            // z - 26764
+            watch101.Stop();
+            Console.WriteLine(watch101.ElapsedMilliseconds);
+
+            var result101Groups = result101
+                .GroupBy(character => character)
+                .Select(group => new { character = group.Key, count = group.Count() })
+                .ToList();
+
+            foreach (var result101Group in result101Groups)
+            {
+                Console.WriteLine($"{result101Group.character} - {result101Group.count}");
+            }
+
+            var watch102 = Stopwatch.StartNew();
+            var result102 = tesolution.GetSmallestString(96109, 1229657);
+            // a - 50767
+            // x - 1
+            // z - 45341
+            watch102.Stop();
+            Console.WriteLine(watch101.ElapsedMilliseconds); 
+
+            var result102Groups = result102
+                .GroupBy(character => character)
+                .Select(group => new { character = group.Key, count = group.Count() })
+                .ToList();
+
+            foreach (var result102Group in result102Groups)
+            {
+                Console.WriteLine($"{result102Group.character} - {result102Group.count}");
+            }
+
+            var result103 = tesolution.GetSmallestString(80, 576);
 
             Console.ReadKey();
         }
@@ -99,7 +164,7 @@ namespace SmallestStringForNumericValue
                  Select(c => (char)c)
                  .ToList();
 
-            var result = String.Empty;
+            var result = new StringBuilder();
 
             while (n > 0)
             {
@@ -109,7 +174,7 @@ namespace SmallestStringForNumericValue
 
                 if (kBalance >= nBalance)
                 {
-                    result = alphabets[chosen - 1] + result;
+                    result.Insert(0, alphabets[chosen - 1]);
 
                     k -= chosen;
                     n--;
@@ -118,21 +183,21 @@ namespace SmallestStringForNumericValue
                 {
                     chosen = k - (n - 1);
 
-                    result = alphabets[chosen - 1] + result;
+                    result.Insert(0, alphabets[chosen - 1]);
                     k -= chosen;
                     n--;
 
-                    while (n > 0)
+                    if (n > 0)
                     {
-                        chosen = 1;
-                        result = alphabets[chosen - 1] + result;
-                        k -= chosen;
-                        n--;
+                        result.Insert(0, new string('a', n));
+
+                        k -= n;
+                        n -= n;
                     }
                 }
             }
 
-            return result;
+            return result.ToString();
         }
     }
 }
