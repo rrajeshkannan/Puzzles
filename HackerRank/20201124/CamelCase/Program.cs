ab4c6e47f1e251b0896133e8a8226ea826624125
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-11-24 10:57:04 +0530

    Problems solved based on string-char manipulation

---
+++ HackerRank/20201124/CamelCase/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CamelCase
+{
+    // https://www.hackerrank.com/challenges/camelcase/problem
+
+    // Alice wrote a sequence of words in CamelCase as a string of letters, , having the following properties:
+    //   It is a concatenation of one or more words consisting of English letters.
+    //   All letters in the first word are lowercase.
+    //   For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
+    //
+    // Given 's', print the number of words in on a new line.
+    //   For example, "s=oneTwoThree". There are 3 words in the string.
+    //
+    // Function Description
+    //   Complete the camelcase function in the editor below.It must return the integer number of words in the input string.
+    //   camelcase has the following parameter(s) :
+    //     s: the string to analyze
+    // Input Format
+    //   A single line containing string s.
+    // Constraints
+    //   1 <= |s| <= 10^5
+    // Output Format
+    //   Print the number of words in string s.
+    //
+    // Sample Input
+    //   saveChangesInTheEditor
+    // Sample Output
+    //   5
+    // Explanation
+    //   String s contains five words:
+    //     save, Changes, In, The, Editor
+    //   Thus, we print 5 on a new line.
+
+    class Solution
+    {
+
+        // Complete the camelcase function below.
+        static int camelcase(string s)
+        {
+            if (String.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            var alphabets = new HashSet<char>(Enumerable.Range('A', 'Z' - 'A' + 1)
+                .Select(letter => (char)letter));
+
+            return s.Count(letter => alphabets.Contains(letter)) + 1;
+        }
+
+        static void Main(string[] args)
+        {
+            var result1 = camelcase("saveChangesInTheEditor"); // 5
+            var result2 = camelcase("save"); // 1
+            var result3 = camelcase("s"); // 1
+            var result4 = camelcase("saveChangesInI"); // 4
+
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            //string s = Console.ReadLine();
+
+            //int result = camelcase(s);
+
+            //textWriter.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
---
+++ HackerRank/20201124/StrongPassword/Program.cs
@@ -0,0 +1,135 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace StrongPassword
+{
+    // https://www.hackerrank.com/challenges/strong-password/problem
+
+    class Solution
+    {
+        static Dictionary<char, LetterKind> _characters= new Dictionary<char, LetterKind>();
+
+        static Solution()
+        {
+            var numbers = Enumerable.Range('0', '9' - '0' + 1)
+                .Select(number => (char)number);
+            foreach (var number in numbers)
+            {
+                _characters.Add(number, LetterKind.Number);
+            }
+
+            var lowerCaseCharacters = Enumerable.Range('a', 'z' - 'a' + 1)
+                .Select(lowerCaseCharacter => (char)lowerCaseCharacter);
+            foreach (var character in lowerCaseCharacters)
+            {
+                _characters.Add(character, LetterKind.LowerCase);
+            }
+
+            var upperCaseCharacters = Enumerable.Range('A', 'Z' - 'A' + 1)
+                .Select(upperCaseCharacter => (char)upperCaseCharacter);
+            foreach (var character in upperCaseCharacters)
+            {
+                _characters.Add(character, LetterKind.UpperCase);
+            }
+
+            var specialCharacters = "!@#$%^&*()-+"
+                .Select(specialCharacter => (char)specialCharacter);
+            foreach (var character in specialCharacters)
+            {
+                _characters.Add(character, LetterKind.SpecialCharacter);
+            }
+        }
+
+
+        enum LetterKind
+        {
+            Unknown,
+            Number,
+            LowerCase,
+            UpperCase,
+            SpecialCharacter
+        }
+
+        const int MinNumbers = 1;
+        const int MinLowerCaseCharacters = 1;
+        const int MinUpperCaseCharacters = 1;
+        const int MinSpecialCharacters = 1;
+
+        // Complete the minimumNumber function below.
+        static int minimumNumber(int n, string password)
+        {
+            var wordGap = 0;
+
+            if (password.Length < 6)
+            {
+                wordGap = 6 - password.Length;
+            }
+
+            // Return the minimum number of characters to make the password strong
+            var result = password
+                .GroupBy(letter => _characters.TryGetValue(letter, out LetterKind letterKind) ? letterKind : LetterKind.Unknown)
+                .ToDictionary(letterGroup => letterGroup.Key, letterGroup => letterGroup.Count());
+            
+            var gap = 0;
+
+            if (!result.TryGetValue (LetterKind.Number, out int numbers))
+            {
+                numbers = 0;
+            }
+            gap += (numbers < MinNumbers) ? (MinNumbers - numbers) : 0;
+
+            if (!result.TryGetValue(LetterKind.LowerCase, out int lowerCaseLetters))
+            {
+                lowerCaseLetters = 0;
+            }
+            gap += (lowerCaseLetters < MinLowerCaseCharacters) ? (MinLowerCaseCharacters - lowerCaseLetters) : 0;
+
+            if (!result.TryGetValue(LetterKind.UpperCase, out int upperCaseLetters))
+            {
+                upperCaseLetters = 0;
+            }
+            gap += (upperCaseLetters < MinUpperCaseCharacters) ? (MinUpperCaseCharacters - upperCaseLetters) : 0;
+
+            if (!result.TryGetValue(LetterKind.SpecialCharacter, out int specialCharacters))
+            {
+                specialCharacters = 0;
+            }
+            gap += (specialCharacters < MinSpecialCharacters) ? (MinSpecialCharacters - specialCharacters) : 0;
+
+            return Math.Max(wordGap, gap);
+        }
+
+        static void Main(string[] args)
+        {
+            var result0 = minimumNumber(3, ""); // 6
+            var result1 = minimumNumber(3, "Ab1"); // 3
+            var result2 = minimumNumber(11, "#HackerRank"); // 1
+            var result3 = minimumNumber(4, "4700"); // 3
+
+
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //string password = Console.ReadLine();
+
+            //int answer = minimumNumber(n, password);
+
+            //textWriter.WriteLine(answer);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
