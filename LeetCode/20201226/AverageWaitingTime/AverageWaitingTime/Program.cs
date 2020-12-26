9c93ed685bf0510bd94f0e05ad6952e9cdd2ce2c
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-12-26 21:21:49 +0530

    Biweekly contest

---
+++ LeetCode/20201226/AverageWaitingTime/AverageWaitingTime/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageWaitingTime
+{
+    public class Solution
+    {
+        public double AverageWaitingTime(int[][] customers)
+        {
+            int[] waitingTimes = new int[customers.Length];
+
+            int chefStartingTime = 1;
+
+            for (int i = 0; i < customers.Length; i++)
+            {
+                int customerArrivalTime = customers[i][0];
+                int preparationTime = customers[i][1];
+
+                int waitingForStartPreparation = 0;
+
+                if (customerArrivalTime < chefStartingTime)
+                {
+                    // customer earlier than chef
+                    waitingForStartPreparation = chefStartingTime - customerArrivalTime;
+                }
+                else
+                {
+                    // chef idle
+                    chefStartingTime = customerArrivalTime;
+                }
+
+                waitingTimes[i] = waitingForStartPreparation + preparationTime;
+                chefStartingTime += preparationTime;
+            }
+
+            return waitingTimes.Average();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var input1 = new int[][]
+            {
+                new int[]{ 1, 2 },
+                new int[]{ 2,5 },
+                new int[]{ 4,3 },
+            };
+
+            var output1 = solution.AverageWaitingTime(input1);
+
+            var input2 = new int[][]
+            {
+                new int[]{ 5, 2 },
+                new int[]{ 5,4 },
+                new int[]{ 10,3 },
+                new int[]{ 20,1 },
+            };
+
+            var output2 = solution.AverageWaitingTime(input2);
+        }
+    }
+}
---
+++ LeetCode/20201226/StudentsLunch/StudentsLunch/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsLunch
+{
+    class Program
+    {
+        public class Solution
+        {
+            public int CountStudents(int[] students, int[] sandwiches)
+            {
+                Queue<int> studentQueue = new Queue<int>(students);
+                Queue<int> unsatisfiedQueue = new Queue<int>();
+
+                for (int i = 0; i < sandwiches.Length; i++)
+                {
+                    var currentSandwich = sandwiches[i];
+
+                    while(studentQueue.Any())
+                    {
+                        var currentStudent = studentQueue.Dequeue();
+
+                        if (currentStudent == currentSandwich)
+                        {
+                            studentQueue = new Queue<int>(studentQueue.Concat(unsatisfiedQueue));
+                            unsatisfiedQueue = new Queue<int>();
+                            break;
+                        }
+
+                        unsatisfiedQueue.Enqueue(currentStudent);
+                    }
+                }
+
+                return unsatisfiedQueue.Count;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var students1 = new int[] { 1, 1, 0, 0 };
+            var sandwiches1 = new int[] { 0, 1, 0, 1 };
+
+            var count1 = solution.CountStudents(students1, sandwiches1);
+
+            var students2 = new int[] { 1, 1, 1, 0, 0, 1 };
+            var sandwiches2 = new int[] { 1, 0, 0, 0, 1, 1 };
+
+            var count2 = solution.CountStudents(students2, sandwiches2);
+
+            var students3 = new int[] { 1, 1, 0, 0 };
+            var sandwiches3 = new int[] { 0, 1, 0, 1 };
+
+            var count3 = solution.CountStudents(students3, sandwiches3);
+        }
+    }
+}
