c9af76242fcba95da172c610a6d90dbbcb642b21
Rajesh Kannan <rrajeshkannan@gmail.com>
2021-01-24 09:36:04 +0530

    Weekly Contest

---
+++ LeetCode/20210124/KthLargestXor/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KthLargestXor
+{
+    // https://leetcode.com/contest/weekly-contest-225/problems/find-kth-largest-xor-coordinate-value/
+
+    public class Solution
+    {
+        public int KthLargestValue(int[][] matrix, int k)
+        {
+            int rows = matrix.Length;
+            int columns = matrix[0].Length;
+            var result = new List<int>();
+
+            for (int m = 0; m < rows; m++)
+            {
+                for (int n = 0; n < columns; n++)
+                {
+                    var value = matrix[0][0];
+
+                    for (int a = 0; a <= m; a++)
+                    {
+                        for (int b = 0; b <= n; b++)
+                        {
+                            if ((a == 0) && (b == 0))
+                                continue;
+
+                            value ^= matrix[a][b];
+                        }
+                    }
+
+                    result.Add(value);
+                }
+            }
+
+            result = result.OrderByDescending(number => number).ToList();
+
+            return result[k - 1];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var input1 = new int[][]
+            {
+                new int[]{ 5, 2 },
+                new int[]{ 1, 6 }
+            };
+            var output1 = solution.KthLargestValue(input1, 1);
+            var output2 = solution.KthLargestValue(input1, 2);
+        }
+    }
+}
---
+++ LeetCode/20210124/LatestTime/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatestTime
+{
+    // https://leetcode.com/contest/weekly-contest-225/problems/latest-time-by-replacing-hidden-digits/
+
+    public class Solution
+    {
+        public string MaximumTime(string time)
+        {
+            var characters = new char[] { '1', '9', ':', '5', '9' };
+
+            var hourFirstAlternateLetter = '2';
+            var hourSecondAlternateLetter = '3';
+
+            var result = new StringBuilder();
+
+            for (int i = 0; i < time.Length; i++)
+            {
+                var actual = time[i];
+
+                if (actual != '?')
+                {
+                    result.Append(actual);
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        var hourActualSecond = time[1];
+
+                        if ((hourActualSecond == '?') 
+                            || (hourActualSecond == '0') 
+                            || (hourActualSecond == '1') 
+                            || (hourActualSecond == '2') 
+                            || (hourActualSecond == '3'))
+                        {
+                            result.Append(hourFirstAlternateLetter);
+                        }
+                        else
+                        {
+                            result.Append(characters[i]);
+                        }
+                    }
+                    else if(i == 1)
+                    {
+                        if (result[0] == '2')
+                        {
+                            result.Append(hourSecondAlternateLetter);
+                        }
+                        else
+                        {
+                            result.Append(characters[i]);
+                        }
+                    }
+                    else
+                    {
+                        result.Append(characters[i]);
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var output1 = solution.MaximumTime("2?:?0");
+            var output2 = solution.MaximumTime("0?:3?");
+            var output3 = solution.MaximumTime("1?:22");
+            var output4 = solution.MaximumTime("?4:03");
+            var output5 = solution.MaximumTime("??:3?");
+        }
+    }
+}
