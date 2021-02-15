8e48d82888946efe38fa650a72242fd4fddfb6c0
Rajesh Kannan <rrajeshkannan@gmail.com>
2021-02-15 10:15:40 +0530

    HackerRank puzzles

---
+++ HackerRank/20210215/FunnyString/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunnyString
+{
+    class Program
+    {
+        // https://www.hackerrank.com/challenges/funny-string/problem
+
+        // Complete the funnyString function below.
+        static string funnyString(string s)
+        {
+            var length = s.Length;
+
+            if (length <= 2)
+            {
+                return "Funny";
+            }
+
+            var iterations = (length - 1) / 2;
+
+            for (int front = 0; front < iterations; front++)
+            {
+                var frontDifference = Math.Abs(s[front] - s[front + 1]);
+                var rear = length - 1 - front;
+                var rearDifference = Math.Abs(s[rear] - s[rear - 1]);
+
+                if (frontDifference != rearDifference)
+                {
+                    return "Not Funny";
+                }
+            }
+
+            return "Funny";
+        }
+
+        static void Main(string[] args)
+        {
+            var output1 = funnyString("lmnop"); // Funny
+            var output2 = funnyString("acxz"); // Funny
+            var output3 = funnyString("bcxz"); // Not Funny
+        }
+    }
+}
---
+++ HackerRank/20210215/Gemstones/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gemstones
+{
+    class Program
+    {
+        // https://www.hackerrank.com/challenges/gem-stones/problem
+
+        // Complete the gemstones function below.
+        static int gemstones(string[] arr)
+        {
+            const char lowerA = 'a';
+
+            var stones = Enumerable.Range(0, 26)
+                .ToDictionary(number => number, number => new int[arr.Length]);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var rock = arr[i];
+
+                for (int j = 0; j < rock.Length; j++)
+                {
+                    var stone = rock[j] - lowerA;
+                    stones[stone][i]++;
+                }
+            }
+
+            return stones.Values
+                .Where(stoneCountInRocks => stoneCountInRocks.All(stoneCountInRock => stoneCountInRock > 0))
+                .Count();
+        }
+
+        static void Main(string[] args)
+        {
+            var input1 = new string[] { "abcdde", "baccd", "eeabg" };
+            var output1 = gemstones(input1); // 2
+        }
+    }
+}
