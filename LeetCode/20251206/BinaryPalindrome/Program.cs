2442f80024a750851ce531f52e5bec1abde81623
Rajesh Kannan <rrajeshkannan@gmail.com>
2025-12-06 23:10:12 +0530

    Puzzles solved during biweekly contest today.

---
+++ LeetCode/20251206/BinaryPalindrome/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+ReadInputsFromCsvFile().ToList().ForEach(input =>
+{
+    var numbers = input.ElementAt(0).ToArray();
+    var minOperations = MinOperations(numbers);
+
+    Console.WriteLine(string.Join(",", minOperations));
+});
+
+int[] MinOperations(int[] nums)
+{
+    var minOperations = new List<int>(0);
+
+    foreach (var number in nums)
+    {
+        var numberForward = number;
+        var minOperationsForward = 0;
+
+        while (!IsBinaryPalindrome(numberForward))
+        {
+            minOperationsForward++;
+            numberForward++;
+        }
+
+        if (minOperationsForward == 0)
+        {
+            minOperations.Add(0);
+            continue;
+        }
+
+        var numberBackward = number;
+        var minOperationsBackward = 0;
+
+        while (numberBackward > 0 && numberBackward < numberForward && !IsBinaryPalindrome(numberBackward))
+        {
+            minOperationsBackward++;
+            numberBackward--;
+        }
+
+        if (numberBackward > 0)
+        {
+            minOperationsForward = Math.Min(minOperationsForward, minOperationsBackward);
+        }
+
+        minOperations.Add(minOperationsForward);
+    }
+
+    return minOperations.ToArray();
+}
+
+bool IsBinaryPalindrome(int number)
+{
+    var binaryString = Convert.ToString(number, 2);
+    var reversedBinaryString = new string(binaryString.Reverse().ToArray());
+    return binaryString == reversedBinaryString;
+}
+
+static IEnumerable<IEnumerable<IEnumerable<int>>> ReadInputsFromCsvFile(int numLinesPerInput = 1)
+{
+    var lines = File.ReadAllLines("inputs.txt");
+    var linesScannedForCurrentInput = 0;
+    var currentInput = new List<IEnumerable<int>> { };
+
+    foreach (var line in lines)
+    {
+        var parts = line.Split(';');
+        var numbers = Array.ConvertAll(parts[0].Split(','), int.Parse);
+
+        currentInput.Add(numbers);
+        linesScannedForCurrentInput++;
+
+        if (linesScannedForCurrentInput == numLinesPerInput)
+        {
+            yield return currentInput;
+            linesScannedForCurrentInput = 0;
+            currentInput = [];
+        }
+    }
+}
---
+++ LeetCode/20251206/CompletePrimeNumber/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace CompletePrimeNumber;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        ReadInputsFromCsvFile().ToList().ForEach(input =>
+        {
+            var number = input.ElementAt(0).First();
+            Console.WriteLine($"{number} is complete prime number: {IsCompletePrimeNumber(number)}");
+        });
+    }
+
+    private static bool IsCompletePrimeNumber(int number)
+    {
+        // Check prefixes
+        var prefix = number;
+        while (prefix != 0)
+        {
+            if (!IsPrime(prefix))
+            {
+                return false;
+            }
+            prefix /= 10;
+        }
+
+        // Check suffixes
+        var suffix = 0;
+        var divisor = 10;
+        while (suffix != number)
+        {
+            suffix = number % divisor;
+            if (!IsPrime(suffix))
+            {
+                return false;
+            }
+            divisor *= 10;
+        }
+
+        return true;
+    }
+
+    private static bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+        for (int i = 3; i <= boundary; i += 2)
+        {
+            if (number % i == 0) return false;
+        }
+
+        return true;
+    }
+
+    public static IEnumerable<IEnumerable<IEnumerable<int>>> ReadInputsFromCsvFile(int numLinesPerInput = 1)
+    {
+        var lines = File.ReadAllLines("inputs.txt");
+        var linesScannedForCurrentInput = 0;
+        var currentInput = new List<IEnumerable<int>> { };
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(';');
+            var numbers = Array.ConvertAll(parts[0].Split(','), int.Parse);
+
+            currentInput.Add(numbers);
+            linesScannedForCurrentInput++;
+
+            if (linesScannedForCurrentInput == numLinesPerInput)
+            {
+                yield return currentInput;
+                linesScannedForCurrentInput = 0;
+                currentInput = [];
+            }
+        }
+    }
+}
