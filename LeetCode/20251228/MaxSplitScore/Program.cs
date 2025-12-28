35fc39542f2724ee0979f49d39284bdb397646a1
Rajesh Kannan <rrajeshkannan@gmail.com>
2025-12-28 09:41:38 +0530

    Puzzles on 2025-12-28

---
+++ LeetCode/20251228/MaxSplitScore/Program.cs
@@ -0,0 +1,35 @@
+﻿var solution = new Solution();
+
+Console.WriteLine(solution.MaximumScore([10, -1, 3, -4, -5]));
+Console.WriteLine(solution.MaximumScore([-7, -5, 3]));
+Console.WriteLine(solution.MaximumScore([1, 1]));
+
+public class Solution
+{
+    public long MaximumScore(int[] nums)
+    {
+        long cumulativeSum = 0;
+        var prefixSums = new long[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            cumulativeSum += nums[i];
+            prefixSums[i] = cumulativeSum;
+        }
+
+        var suffixMins = new long[nums.Length];
+        long currentMin = nums[nums.Length - 1];
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            suffixMins[i] = currentMin;
+            currentMin = Math.Min(currentMin, nums[i]);
+        }
+
+        long score = long.MinValue;
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            score = Math.Max(score, prefixSums[i] - suffixMins[i]);
+        }
+
+        return score;
+    }
+}
---
+++ LeetCode/20251228/SmallestAllOnesMultiple/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+var solution = new Solution();
+
+Stopwatch stopwatch2 = Stopwatch.StartNew();
+Stopwatch stopwatch1 = Stopwatch.StartNew();
+Console.WriteLine($"Result: {solution.MinAllOneMultiple(3)}, Time: {stopwatch1.ElapsedMilliseconds}");
+stopwatch1.Restart();
+Console.WriteLine($"Result: {solution.MinAllOneMultiple(7)}, Time: {stopwatch1.ElapsedMilliseconds}");
+stopwatch1.Restart();
+Console.WriteLine($"Result: {solution.MinAllOneMultiple(2)}, Time: {stopwatch1.ElapsedMilliseconds}");
+stopwatch1.Restart();
+Console.WriteLine($"Result: {solution.MinAllOneMultiple(9)}, Time: {stopwatch1.ElapsedMilliseconds}");
+stopwatch1.Restart();
+Console.WriteLine($"Result: {solution.MinAllOneMultiple(17)}, Time: {stopwatch1.ElapsedMilliseconds}");
+stopwatch1.Restart();
+Console.WriteLine($"Result: {solution.MinAllOneMultiple(23)}, Time: {stopwatch1.ElapsedMilliseconds}");
+stopwatch1.Restart();
+Console.WriteLine($"Result: {solution.MinAllOneMultiple(313)}, Time: {stopwatch1.ElapsedMilliseconds}");
+stopwatch1.Restart();
+Console.WriteLine($"Result: {solution.MinAllOneMultiple(79251)}, Time: {stopwatch1.ElapsedMilliseconds}");
+stopwatch1.Restart();
+Console.WriteLine($"Total Time: {stopwatch2.ElapsedMilliseconds}");
+
+
+// Solved using RepUnit 
+
+public class Solution
+{
+    public int MinAllOneMultiple(int k)
+    {
+        if (k % 2 == 0 || k % 5 == 0)
+            return -1;
+
+        int remainder = 1 % k;
+        int length = 1;
+
+        while (remainder != 0)
+        {
+            remainder = (remainder * 10 + 1) % k;
+            length++;
+        }
+
+        return length;
+    }
+}
